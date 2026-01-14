06991a41f2752341a8364907f540b8677f4bb7bb
sridharpettelapersonal <sridharpettela83@gmail.com>
2026-01-13 21:06:43 -0500

    changes

---
+++ MCS/MCS.WebApi/DTOs/Auth/LoginDto.cs
@@ -0,0 +1,11 @@
+namespace MCS.WebApi.DTOs.Auth
+{
+    public class LoginDto
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+
+   
+}
+
---
+++ MCS/MCS.WebApi/DTOs/Organization/OrganizationWithBranchDto.cs
@@ -0,0 +1,18 @@
+﻿using MCS.WebApi.DTOs.Branch;
+
+namespace MCS.WebApi.DTOs.Organization
+{
+    public class OrganizationWithBranchDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Address1 { get; set; }
+        public string? Address2 { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public string? Country { get; set; }
+        public string? ZipCode { get; set; }
+        public string? PhoneNumber { get; set; }
+        public List<BranchInfoDto> Branches { get; set; } = new();
+    }
+}
