4fd208a23c8b8dbe3da37a7991d4cf24c0df05fd
sridharpettela <sridharpettela@dinspire.in>
2026-01-17 19:34:11 -0500

    HealthController

---
+++ MCS/MCS.WebApi/Controllers/HealthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MCS.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/health")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("API is running");
+    }
+}
