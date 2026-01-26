ef9e9215a637dcb7efe45e0539c51d0170f5a1c9
Just-hari <hariprasadpulibandla@gmail.com>
2026-01-26 17:26:07 -0500

    updated name as mandatory

--- MCS/MCS.WebApi/DTOs/CreateCenterDto.cs
+++ MCS/MCS.WebApi/DTOs/CreateCenterDto.cs
@@ -1,8 +1,10 @@
-﻿namespace MCS.WebApi.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MCS.WebApi.DTOs
 {
     public class CreateCenterDto
     {
-       
+        [Required]
         public string Name { get; set; } = string.Empty;
         public string? City { get; set; }
         public string? CenterAddress { get; set; }
