32b3e7c2ce27e27bfe60b65b8a2e3466687891f7
sridharpettela <sridharpettela@dinspire.in>
2026-02-15 13:05:35 -0500

    changes

---
+++ MCS/MCS.WebApi/Migrations/20260215180503_LoanSchedulers-Update-1.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MCS.WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class LoanSchedulersUpdate1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "CollectedBy",
+                table: "LoanSchedulers",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "ActualEmiAmount",
+                table: "LoanSchedulers",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "ActualInterestAmount",
+                table: "LoanSchedulers",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "ActualPrincipalAmount",
+                table: "LoanSchedulers",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ActualEmiAmount",
+                table: "LoanSchedulers");
+
+            migrationBuilder.DropColumn(
+                name: "ActualInterestAmount",
+                table: "LoanSchedulers");
+
+            migrationBuilder.DropColumn(
+                name: "ActualPrincipalAmount",
+                table: "LoanSchedulers");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CollectedBy",
+                table: "LoanSchedulers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
